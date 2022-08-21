1ec64ca1f82203ed12099a999d9cd3b2073a8288
Carlo <hola.carlodominguez@gmail.com>
2022-08-21 20:55:52 +0200

    chore: renames some files

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,10 +1,7 @@
-import { Client, Message } from "discord.js"
+import { Client, Message, MessagePayload, MessageOptions } from "discord.js"
 
 import { CHANNEL_ID } from "./constants"
 
-/**
- * Gets all messages from a text channel.
- */
 export async function getAllChannelMessages({
   client,
   id,
@@ -39,4 +36,22 @@ export async function getAllChannelMessages({
   }
 
   return messages
+}
+
+export async function sendMessageToChannel(
+  client: Client,
+  id: CHANNEL_ID,
+  message: string | MessagePayload | MessageOptions
+) {
+  const channel = client.channels.cache.find((channel) => channel.id === id)
+
+  if (!channel) {
+    throw Error(`could not find channel ${id}`)
+  }
+
+  if (!channel.isTextBased()) {
+    throw Error(`channel ${id} is not text based`)
+  }
+
+  return channel.send(message)
 }
