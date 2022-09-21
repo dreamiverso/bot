9006feafa15f6f9071df38af11081a67d5402d23
Carlo <hola.carlodominguez@gmail.com>
2022-09-21 21:20:52 +0200

    feat: ports icons command from the original bot

---
+++ src/features/icons/commands/icons.ts
@@ -0,0 +1,20 @@
+import { oneLine } from "common-tags"
+import { SlashCommandBuilder } from "discord.js"
+
+import { createCommand } from "~/utils"
+
+const content = oneLine`
+  Aquí puedes consultar la lista de iconos oficial de Dreams: \n
+  https://docs.indreams.me/es/help/getting-started/icons
+`
+
+const builder = new SlashCommandBuilder()
+  .setName("iconos")
+  .setDescription("Devuelve un enlace a la lista de iconos oficial de Dreams.")
+
+export default createCommand(builder, (interaction) => {
+  interaction.reply({
+    ephemeral: true,
+    content,
+  })
+})
