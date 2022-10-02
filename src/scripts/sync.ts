6980618e94a0bb3deb4d9d5ce67e034dab6ab263
Carlo <hola.carlodominguez@gmail.com>
2022-10-02 13:40:34 +0200

    fix: prevents crashes when syncing commented command files

--- src/scripts/sync.ts
+++ src/scripts/sync.ts
@@ -9,10 +9,12 @@ import { CreateCommandResult, env } from "~/utils"
 const featuresRoute = path.join(__dirname, "../features")
 const commandsFiles = glob.sync(`${featuresRoute}/**/command.*.ts`)
 
-const body = commandsFiles.map((route) => {
-  const command = require(route).default as CreateCommandResult
-  return command.builder.toJSON()
-})
+const body = commandsFiles
+  .map((route) => {
+    const command = require(route).default as CreateCommandResult
+    return command.builder.toJSON()
+  })
+  .filter((body) => body)
 
 const rest = new REST({ version: "10" }).setToken(env.DISCORD_BOT_TOKEN)
 
